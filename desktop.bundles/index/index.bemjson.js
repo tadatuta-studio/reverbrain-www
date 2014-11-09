8378a9926bcd8da5c6d6680b5ad9640f4889c370
Григорий Гриненко <grigoriy@tadatuta.com>
2014-11-10 02:25:20 +0300

    fix favicon path

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -1,7 +1,7 @@
 ({
     block : 'page',
     title : 'Reverbrain',
-    favicon : '/favicon.ico',
+    favicon : 'favicon.ico',
     head : [
         { elem : 'meta', attrs : { name : 'description', content : '' } },
         { elem : 'meta', attrs : { name : 'viewport', content : 'width=device-width, initial-scale=1' } },
