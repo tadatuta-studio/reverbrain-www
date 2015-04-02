445bd2409bde9b3637814e8c17a770c81f40328e
Grigoriy Grinenko <grigoriy@tadatuta.com>
2015-04-02 22:56:28 +0300

    revert build system

--- build.sh
+++ build.sh
@@ -1,24 +1,12 @@
-# Скрипт для сборки сайта в конкретную директорию
-
-npm i
-./node_modules/.bin/bower-npm-install
-
-# собранный сайт
 OUTPUT_DIR=output
 
-# пересобрать
-./node_modules/.bin/enb make clean
-YENV=production ./node_modules/.bin/enb make
-
-# удалить старую сборку
-rm -rf $OUTPUT_DIR
-mkdir $OUTPUT_DIR
-
-# деплой
+YENV=production ./node_modules/.bin/bem make
+mkdir -p $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR/i
 cp desktop.bundles/index/index.html $OUTPUT_DIR/
 cp desktop.bundles/index/_index.css $OUTPUT_DIR/
 cp desktop.bundles/index/_index.js $OUTPUT_DIR/
-cp -r desktop.bundles/index/i $OUTPUT_DIR/
+cp -r i/* $OUTPUT_DIR/i/
 cp favicon.ico $OUTPUT_DIR/
-
 touch $OUTPUT_DIR/.nojekyll
+# echo "reverbrain.com" >> $OUTPUT_DIR/CNAME
